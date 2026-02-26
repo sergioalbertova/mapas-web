732a7845f2bf4c60951b8d88079c072c17a7e20a
sergioalbertova <sergio_albertov@hotmail.com>
2026-02-26 09:17:46 -0600

    Create login.js

---
+++ public/login.js
@@ -0,0 +1,24 @@
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const res = await fetch("login.php", {
+        method: "POST",
+        body: formData
+    });
+
+    const data = await res.json();
+    const msg = document.getElementById("msg");
+
+    if (data.status === "success") {
+        msg.textContent = "Acceso correcto, redirigiendo...";
+        msg.style.color = "green";
+        setTimeout(() => {
+            window.location.href = "dashboard.php";
+        }, 1000);
+    } else {
+        msg.textContent = data.message;
+        msg.style.color = "red";
+    }
+});
