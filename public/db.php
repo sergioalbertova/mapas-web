a4151eba7fc7060307b701da5e03809478b43fbf
sergioalbertova <sergio_albertov@hotmail.com>
2026-02-26 11:21:46 -0600

    Update db.php

--- public/db.php
+++ public/db.php
@@ -2,15 +2,21 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$host = "ep-xxxxx.us-east-2.aws.neon.tech";
-$dbname = "neon_db";
-$user = "neon_user";
+$host = "ep-cold-dawn-aitfe8k8-pooler.c-4.us-east-1.aws.neon.tech";
+$dbname = "neondb";
+$user = "neondb_owner";
 $pass = "npg_uZAv9VetBzl4";
 
 try {
-    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
+    $pdo = new PDO(
+        "pgsql:host=$host;dbname=$dbname;sslmode=require",
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    );
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
+
