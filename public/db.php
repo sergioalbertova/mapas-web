759c05b60b0611eaeb75bebde3fb3f16104ec68f
sergioalbertova <sergio_albertov@hotmail.com>
2026-02-26 11:09:02 -0600

    Update db.php

--- public/db.php
+++ public/db.php
@@ -1,8 +1,11 @@
 <?php
-$host = "TU_HOST";
-$dbname = "TU_DB";
-$user = "TU_USUARIO";
-$pass = "TU_PASSWORD";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$host = "ep-xxxxx.us-east-2.aws.neon.tech";
+$dbname = "neon_db";
+$user = "neon_user";
+$pass = "npg_uZAv9VetBzl4";
 
 try {
     $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass, [
