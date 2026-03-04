60314572621f21d03e54654de0a568b932b34f6d
sergioalbertova <sergio_albertov@hotmail.com>
2026-03-04 15:01:42 -0600

    Update dashboard.php

--- public/dashboard.php
+++ public/dashboard.php
@@ -220,7 +220,7 @@ let zoom = 1, posX = 0, posY = 0, dragging = false, lastX = 0, lastY = 0;
 
 /* 🔧 CALIBRACIÓN DEL MARCADOR */
 let offsetX = 15;   // Ajusta horizontal (+ derecha, - izquierda)
-let offsetY = -200;   // Ajusta vertical (+ abajo, - arriba)
+let offsetY = -225;   // Ajusta vertical (+ abajo, - arriba)
 let scaleX = 1;    // Ajusta proporción horizontal
 let scaleY = 1;    // Ajusta proporción vertical
 
