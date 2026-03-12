1f2473729bf244622f25cf989dc93266ca7670c2
sergioalbertova <sergio_albertov@hotmail.com>
2026-03-12 14:53:57 -0600

    Update itil_incidentes.php

--- public/itil_incidentes.php
+++ public/itil_incidentes.php
@@ -371,6 +371,10 @@ function toggleTheme() {
     document.body.classList.toggle("dark");
     localStorage.setItem("theme", document.body.classList.contains("dark") ? "dark" : "light");
 }
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+}
+
 
 if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark");
