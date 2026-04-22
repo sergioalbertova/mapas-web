387e4ee8c3d059dbdebb3e5707ff1ee7777c054f
sergioalbertova <sergio_albertov@hotmail.com>
2026-04-22 10:20:01 -0600

    Update itil_catalogo_ver.php

--- public/itil_catalogo_ver.php
+++ public/itil_catalogo_ver.php
@@ -8,7 +8,7 @@ if (!isset($_GET['id'])) {
 
 $id = intval($_GET['id']);
 
-$stmt = $pdo->prepare("SELECT * FROM itil_catalogo WHERE id = ?");
+$stmt = $pdo->prepare("SELECT * FROM catapoyo WHERE id = ?");
 $stmt->execute([$id]);
 $apoyo = $stmt->fetch(PDO::FETCH_ASSOC);
 
