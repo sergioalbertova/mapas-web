2fc82f4c2f9e42c2118463209b0d97d1dcbe8aa6
sergioalbertova <sergio_albertov@hotmail.com>
2026-04-24 14:46:33 -0600

    c2

--- public/login2.php
+++ public/login2.php
@@ -13,6 +13,7 @@ if (isset($_SESSION['user_id'])) {
     <link rel="stylesheet" href="login2.css">
 </head>
 <body>
+<canvas id="particles"></canvas>
 
 <div class="login-wrapper">
     <div class="login-illustration">
@@ -57,5 +58,46 @@ if (isset($_SESSION['user_id'])) {
 </div>
 
 <script src="login.js"></script>
+<script>
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
+for (let i = 0; i < 80; i++) {
+    particles.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        r: Math.random() * 2 + 1,
+        dx: (Math.random() - 0.5) * 0.6,
+        dy: (Math.random() - 0.5) * 0.6
+    });
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach(p => {
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2);
+        ctx.fillStyle = "rgba(255,255,255,0.7)";
+        ctx.fill();
+
+        p.x += p.dx;
+        p.y += p.dy;
+
+        if (p.x < 0 || p.x > canvas.width) p.dx *= -1;
+        if (p.y < 0 || p.y > canvas.height) p.dy *= -1;
+    });
+
+    requestAnimationFrame(animate);
+}
+
+animate();
+</script>
+
 </body>
 </html>
