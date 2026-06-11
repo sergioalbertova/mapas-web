f045886b64fdd2cd4b59d9b955b9e09388b26295
elyomero-valenzu <elyomero@gmail.com>
2026-06-10 20:54:17 -0600

    Update buscar_activeuser.php

--- public/buscar_activeuser.php
+++ public/buscar_activeuser.php
@@ -3,13 +3,24 @@ require "db.php";
 
 $q = $_GET['q'] ?? '';
 
-$stmt = $pdo->prepare("
-    SELECT idu, nomuser, ubicacion
+$sql = "
+    SELECT *
     FROM activeuser
-    WHERE nomuser ILIKE ?
-    ORDER BY nomuser
-    LIMIT 20
-");
-$stmt->execute(["%$q%"]);
+    WHERE 
+        nomuser ILIKE ? OR
+        ubicacion ILIKE ? OR
+        hor1 ILIKE ? OR
+        hor2 ILIKE ?
+    ORDER BY nomuser ASC
+";
 
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+$stmt = $pdo->prepare($sql);
+
+$param = "%$q%";
+
+$stmt->execute([$param, $param, $param, $param]);
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+header('Content-Type: application/json');
+echo json_encode($results);
