53c4af6f2dbb64f39694b327e6dcd66d0cced878
elyomero-valenzu <elyomero@gmail.com>
2026-06-12 10:06:16 -0600

    Update get_ubicaciones.php

--- public/get_ubicaciones.php
+++ public/get_ubicaciones.php
@@ -3,7 +3,7 @@ require "db.php";
 
 $piso = $_GET['piso'];
 
-$stmt = $pdo->prepare("SELECT idubicacion, ubicacion FROM ubicacion WHERE piso = ?");
+$stmt = $pdo->prepare("SELECT idubicacion, ubicacion FROM ubicacion WHERE piso = ? order by ubicacion");
 $stmt->execute([$piso]);
 
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
