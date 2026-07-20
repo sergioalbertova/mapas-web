9eca10ea9dc3bb36f46b19d83f60f0d635dfd64f
sergioalbertova <sergio_albertov@hotmail.com>
2026-07-20 12:01:21 -0600

    Update calendario.php

--- public/calendario.php
+++ public/calendario.php
@@ -400,7 +400,7 @@ body.dark .icono-guardia {
 
 <h1>
 
-<svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+<svg width="26" height="26" viewBox="0 0 24 24" fill="currentColor">
     <path d="M19 4h-1V2h-2v2H8V2H6v2H5a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 15H5V9h14v10zm0-12H5V6h14v1z"/>
 </svg>
 
