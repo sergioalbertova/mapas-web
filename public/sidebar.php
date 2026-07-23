3592230a0b3ed49471a905bd381bd4de4a06fa2d
sergioalbertova <sergio_albertov@hotmail.com>
2026-07-23 10:13:56 -0600

    c2

--- public/sidebar.php
+++ public/sidebar.php
@@ -9,53 +9,53 @@
         <li class="nav-item">
             <a href="index.php">
                 <svg viewBox="0 0 24 24">
-                    <path d="M10 2L2 8h2v8h4V12h4v4h4V8h2z"/>
+                    <path d="M10 2L2 8h2v8h4V12h4v4h4V8h2z" />
                 </svg>
                 <span class="nav-text">Inicio</span>
             </a>
-            
+
             <span class="tooltip">
                 Inicio
-         </span>
+            </span>
 
         </li>
 
         <li class="nav-item">
             <a href="itil_incidentes.php">
                 <svg viewBox="0 0 24 24">
-                    <path d="M4 4h16v4H4V4zm0 6h16v10H4V10zm4 2v2h8v-2H8z"/>
+                    <path d="M4 4h16v4H4V4zm0 6h16v10H4V10zm4 2v2h8v-2H8z" />
                 </svg>
                 <span class="nav-text">Incidentes / Apoyos</span>
             </a>
 
-         <span class="tooltip">
+            <span class="tooltip">
                 Incidentes / Apoyos
-         </span>
+            </span>
 
         </li>
 
         <li class="nav-item">
             <a href="dashboard.php">
                 <svg viewBox="0 0 24 24">
-                    <path d="M3 3h8v8H3V3zm10 0h8v5h-8V3zM3 13h5v8H3v-8zm7 0h11v8H10v-8z"/>
+                    <path d="M3 3h8v8H3V3zm10 0h8v5h-8V3zM3 13h5v8H3v-8zm7 0h11v8H10v-8z" />
                 </svg>
                 <span class="nav-text">Mapeo de nodos</span>
             </a>
             <span class="tooltip">
                 Mapeo de nodos
-         </span>
+            </span>
 
         </li>
 
         <li class="nav-item">
             <a href="calendario.php">
                 <svg viewBox="0 0 24 24">
-                    <path d="M6 2v2H4v2h12V4h-2V2h-2v2H8V2H6zm12 6H2v10h16V8z"/>
+                    <path d="M6 2v2H4v2h12V4h-2V2h-2v2H8V2H6zm12 6H2v10h16V8z" />
                 </svg>
                 <span class="nav-text">Calendario</span>
             </a>
 
-             <span class="tooltip">
+            <span class="tooltip">
                 Calendario
             </span>
 
@@ -65,14 +65,14 @@
         <li class="nav-item">
             <a href="incidentes.php">
                 <svg viewBox="0 0 24 24">
-                    <path d="M4 4h16v4H4V4zm0 6h16v10H4V10zm4 2v2h8v-2H8z"/>
+                    <path d="M4 4h16v4H4V4zm0 6h16v10H4V10zm4 2v2h8v-2H8z" />
                 </svg>
                 <span class="nav-text">Incidentes TI</span>
             </a>
 
-             <span class="tooltip">
+            <span class="tooltip">
                 Incidentes TI
-         </span>
+            </span>
 
 
         </li>
@@ -81,106 +81,112 @@
         <li class="nav-item">
             <a href="actividades_extras.php">
                 <svg viewBox="0 0 24 24">
-                <path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1s-2.4.84-2.82 2H5a2 2 0 00-2 2v14a2 
+                    <path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1s-2.4.84-2.82 2H5a2 2 0 00-2 2v14a2 
                      2 0 002 2h14a2 2 0 002-2V5a2 2 0 00-2-2zm-7 0c.55 0 1 .45 1 1s-.45 
-                     1-1 1-1-.45-1-1 .45-1 1-1zm0 16H7v-2h5v2zm5-4H7v-2h10v2zm0-4H7V9h10v2z"/>
+                     1-1 1-1-.45-1-1 .45-1 1-1zm0 16H7v-2h5v2zm5-4H7v-2h10v2zm0-4H7V9h10v2z" />
                 </svg>
                 <span class="nav-text">Actividades Extras</span>
             </a>
-        <span class="tooltip">
+            <span class="tooltip">
                 Actividades Extras
-         </span>
+            </span>
 
         </li>
 
-       <li class="nav-item submenu">
+        <li class="nav-item submenu">
 
-    <a class="submenu-toggle">
+            <a class="submenu-toggle">
 
-        <svg viewBox="0 0 24 24">
-            <path d="M4 4h16v4H4V4zm0 6h16v4H4v-4zm0 6h16v4H4v-4z"/>
-        </svg>
+                <svg viewBox="0 0 24 24">
+                    <path d="M4 4h16v4H4V4zm0 6h16v4H4v-4zm0 6h16v4H4v-4z" />
+                </svg>
 
-        <span class="nav-text">Respaldos</span>
+                <span class="nav-text">Respaldos</span>
 
-    </a>
+            </a>
 
-        <span class="tooltip">
+            <span class="tooltip">
                 Respaldos
-         </span>
+            </span>
 
-    <ul class="submenu-items">
+            <ul class="submenu-items">
 
-        <li>
-            <a href="consulta_respaldos.php">
-                <span class="nav-text">Consulta</span>
-            </a>
+
+                <li class="submenu-title">
+                    Respaldos
+                </li>
+
+
+                <li>
+                    <a href="consulta_respaldos.php">
+                        <span class="nav-text">Consulta</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="respaldos_usuarios.php">
+                        <span class="nav-text">Administración de Respaldos</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="discos_respaldo.php">
+                        <span class="nav-text">Discos</span>
+                    </a>
+                </li>
+
+            </ul>
+
         </li>
-
-        <li>
-            <a href="respaldos_usuarios.php">
-                <span class="nav-text">Administración de Respaldos</span>
-            </a>
-        </li>
-
-        <li>
-            <a href="discos_respaldo.php">
-                <span class="nav-text">Discos</span>
-            </a>
-        </li>
-
-    </ul>
-
-</li>
 
 
 
 
 
         <li class="nav-item">
-                <a href="activeuser_admin.php">
-                    <svg viewBox="0 0 24 24">
-                        <path d="M12 2l8 4v6c0 5-3.5 9.7-8 10-4.5-.3-8-5-8-10V6l8-4zm-1 13l5-5-1.4-1.4L11 12.2l-1.6-1.6L8 12l3 3z"/>
-                    </svg>
-                    <span class="nav-text">Usuarios</span>
-                </a>
+            <a href="activeuser_admin.php">
+                <svg viewBox="0 0 24 24">
+                    <path d="M12 2l8 4v6c0 5-3.5 9.7-8 10-4.5-.3-8-5-8-10V6l8-4zm-1 13l5-5-1.4-1.4L11 12.2l-1.6-1.6L8 12l3 3z" />
+                </svg>
+                <span class="nav-text">Usuarios</span>
+            </a>
 
-                <span class="tooltip">
+            <span class="tooltip">
                 Usuarios
-                </span>
+            </span>
 
-            </li>
-       
+        </li>
+
 
 
         <li class="nav-item">
             <a href="cambiar_password.php">
                 <svg viewBox="0 0 24 24">
-                    <path d="M12 1a5 5 0 00-5 5v3H5v10h14V9h-2V6a5 5 0 00-5-5zm-3 5a3 3 0 016 0v3H9V6zm1 6h4v6h-4v-6z"/>
+                    <path d="M12 1a5 5 0 00-5 5v3H5v10h14V9h-2V6a5 5 0 00-5-5zm-3 5a3 3 0 016 0v3H9V6zm1 6h4v6h-4v-6z" />
                 </svg>
                 <span class="nav-text">Cambiar contraseña</span>
             </a>
 
-             <span class="tooltip">
-               Cambiar contraseña
+            <span class="tooltip">
+                Cambiar contraseña
             </span>
 
         </li>
 
-       
-            
+
+
 
 
         <li class="nav-item">
             <a href="logout.php">
                 <svg viewBox="0 0 24 24">
-                    <path d="M16 13v-2H7V8l-5 4 5 4v-3h9zm2-10H8v2h10v14H8v2h10a2 2 0 002-2V5a2 2 0 00-2-2z"/>
+                    <path d="M16 13v-2H7V8l-5 4 5 4v-3h9zm2-10H8v2h10v14H8v2h10a2 2 0 002-2V5a2 2 0 00-2-2z" />
                 </svg>
                 <span class="nav-text">Cerrar sesión</span>
             </a>
 
             <span class="tooltip">
-              Cerrar sesión
+                Cerrar sesión
             </span>
 
         </li>
@@ -191,24 +197,22 @@
 
 
 <script>
+    document.addEventListener('DOMContentLoaded', () => {
 
-document.addEventListener('DOMContentLoaded', () => {
+        const submenuToggle = document.querySelector('.submenu-toggle');
+        const submenu = document.querySelector('.submenu');
 
-    const submenuToggle = document.querySelector('.submenu-toggle');
-    const submenu = document.querySelector('.submenu');
+        if (submenuToggle && submenu) {
 
-    if (submenuToggle && submenu) {
+            submenuToggle.addEventListener('click', (e) => {
 
-        submenuToggle.addEventListener('click', (e) => {
+                e.preventDefault();
 
-            e.preventDefault();
+                submenu.classList.toggle('open');
 
-            submenu.classList.toggle('open');
+            });
 
-        });
+        }
 
-    }
-
-});
-
+    });
 </script>
