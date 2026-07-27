0967a7e1055327d05554b176341a679004db22f0
sergioalbertova <sergio_albertov@hotmail.com>
2026-07-27 12:57:14 -0600

    Update itil_problemas.php

--- public/itil_problemas.php
+++ public/itil_problemas.php
@@ -339,6 +339,26 @@ $paginaActual = basename($_SERVER['PHP_SELF']);
                 0 10px 25px var(--shadow);
 
         }
+
+        .main-shell {
+
+            margin-left: 240px;
+
+            width: calc(100% - 240px);
+
+            transition:
+                margin-left .25s ease,
+                width .25s ease;
+
+        }
+
+        #sidebar.collapsed~.main-shell {
+
+            margin-left: 70px;
+
+            width: calc(100% - 70px);
+
+        }
     </style>
     <link rel="stylesheet" href="sidebar.css">
     <link rel="stylesheet" href="topbar.css">
